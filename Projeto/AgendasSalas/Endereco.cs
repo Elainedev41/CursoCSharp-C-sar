6acd1a86215719f316a395ed4177bcf9e5b329b4
Elaine <elainedevjava@gmail.com>
2022-10-03 12:28:06 -0300

    Adição da pasta de projetos

---
+++ Projeto/AgendasSalas/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendasSalas
+{
+    internal class Endereco
+    {
+        string _logradouro;
+        string _bairro;
+        string _numero;
+        string _complemento;
+    }
+}
---
+++ Projeto/AgendasSalas/Reserva.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendasSalas
+{
+    internal class Reserva
+    {
+        List<DisponibilidadeSala> _salasAlugadas;
+        List<Equipamento> _equipamentos;
+        List<Servico> _servicos;
+        List<Objetos> _objetos;
+
+        public bool RealizarContratacao()
+        {
+            return true;
+        }
+
+    }
+}
